0b137e5e53446816e29671a3d6374062732fe69d
Richmond Dominguez <richmonddominguez@gmail.com>
2022-04-13 00:22:52 -0700

    first push of starter code. Added to html,css and js, rough html format and css styling, start to questions for the quiz in js

---
+++ script.js
@@ -0,0 +1,58 @@
+var questions = [
+  {
+    numb: 1,
+    question: "What type of Pokemon is Pikachu?",
+    answer: "Electric Type",
+    options: ["Water Type", "Ground Type", "Fire Type", "Electric Type"],
+  },
+  {
+    numb: 2,
+    question:
+      "What item is used to log a trainers Pokemon encounters along their journey?",
+    answer: "A Pokedex",
+    options: ["A Pokedex", "A Pokelist", "A Pokepad", "A Pokebowl"],
+  },
+  {
+    numb: 3,
+    question: "What item do you aquire after defeating a gym leader?",
+    answer: "A badge",
+    options: ["A keycard", "A pat on the back", "A badge", "A Pokenet"],
+  },
+  {
+    numb: 4,
+    question:
+      "What are the 3 starter pokemon a trainer is presented with at the start of their journey?",
+    answer: "Charmander, Bulbasaur, Squirtle",
+    options: [
+      "Digglet, Geodude, Onyx",
+      "Weedle, Gastly, Mew",
+      "Charmander, Bulbasaur, Squirtle",
+      "Gyarados, Dragonite, Eevee",
+    ],
+  },
+  {
+    numb: 5,
+    question: "What is the item used to capture a Pokemon?",
+    answer: "A Pokeball",
+    options: ["A Pokebowl", "A Pokecage", "A Poketrap", "A Pokeball"],
+  },
+  {
+    numb: 6,
+    question: "What type is most effective towards a fire type Pokemon?",
+    answer: "A water type",
+    options: ["A water type", "A grass type", "A flying type", "A bug type"],
+  },
+  {
+    numb: 7,
+    question: "Where do you go to heal and revive your Pokemon after a battle?",
+    answer: "A Pokecenter",
+    options: ["A Pokemart", "A Pokecenter", "A Pokemall", "A Pokehospital"],
+  },
+  {
+    numb: 8,
+    question:
+      "Who is the main character/trainer in the Pokemon series? (1990s)",
+    answer: "Ash",
+    options: ["Ash", "Asher", "Ashley", "Ashy Knees"],
+  },
+];
